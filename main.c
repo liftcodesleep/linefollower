c8c602e6f259f8caacf06010cb40204cb2e3b20e
liftcodesleep <liftcodesleep@gmail.com>
2023-11-28 14:24:34 -0800

    Update main.c

--- main.c
+++ main.c
@@ -10,51 +10,91 @@
 #define PWM_CHANNEL_A PCA_CHANNEL_0
 #define M1_REV_CHANNEL PCA_CHANNEL_1
 #define M1_FWD_CHANNEL PCA_CHANNEL_2
-#define PWM_CHANNEL_B PCA_CHANNEL_5
 #define M2_FWD_CHANNEL PCA_CHANNEL_3
 #define M2_REV_CHANNEL PCA_CHANNEL_4
+#define PWM_CHANNEL_B PCA_CHANNEL_5
 
 int pressed = 0;
 
+// Function to accelerate motors forward
+void accelerate_forward() {
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 100);
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 100);
+  PCA9685_SetLevel(M1_FWD_CHANNEL, 1);
+  PCA9685_SetLevel(M1_REV_CHANNEL, 0);
+  PCA9685_SetLevel(M2_FWD_CHANNEL, 1);
+  PCA9685_SetLevel(M2_REV_CHANNEL, 0);
+}
+
+// Function to accelerate motors backward
+void accelerate_backward() {
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 100);
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 100);
+  PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M1_REV_CHANNEL, 1);
+  PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M2_REV_CHANNEL, 1);
+}
+
+// Function to turn left
+void turn_left() {
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 100);
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 100);
+  PCA9685_SetLevel(M1_FWD_CHANNEL, 1);
+  PCA9685_SetLevel(M1_REV_CHANNEL, 0);
+  PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M2_REV_CHANNEL, 1);
+}
+
+// Function to turn right
+void turn_right() {
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 100);
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 100);
+  PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M1_REV_CHANNEL, 1);
+  PCA9685_SetLevel(M2_FWD_CHANNEL, 1);
+  PCA9685_SetLevel(M2_REV_CHANNEL, 0);
+}
+
+// Function to stop motors
+void stop() {
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 0);
+  PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 0);
+  PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
+  PCA9685_SetLevel(M1_REV_CHANNEL, 0);
+  PCA9685_SetLevel(M2_REV_CHANNEL, 0);
+}
+
 // Function to control the motors as per the specified sequence
-void MotorControlSequence(int pin, int pin_state, uint32_t time) {
+void motor_control_sequence(int pin, int pin_state, uint32_t time) {
   if (!pressed && pin_state == 1) {
     // Accelerate both motors forward
     printf("Accelerating both motors forward to top speed\n");
-    PCA9685_SetPwmDutyCycle(PWM_CHANNEL_A, 100);
-    PCA9685_SetPwmDutyCycle(PWM_CHANNEL_B, 100);
-    PCA9685_SetLevel(M1_FWD_CHANNEL, 1);
-    PCA9685_SetLevel(M1_REV_CHANNEL, 0);
-    PCA9685_SetLevel(M2_FWD_CHANNEL, 1);
-    PCA9685_SetLevel(M2_REV_CHANNEL, 0);
-
+    accelerate_forward();
     usleep(1000000);  // Sleep for 1 second
 
     // Stop both motors
     printf("Stopping both motors\n");
-    PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M1_REV_CHANNEL, 0);
-    PCA9685_SetLevel(M2_REV_CHANNEL, 0);
-
+    stop();
     usleep(1000000);  // Sleep for 1 second
 
     // Accelerate both motors in reverse
     printf("Accelerating both motors in reverse to top speed\n");
-    PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M1_REV_CHANNEL, 1);
-    PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M2_REV_CHANNEL, 1);
-
+    accelerate_backward();
     usleep(1000000);  // Sleep for 1 second
 
     // Stop both motors
     printf("Stopping both motors\n");
-    PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
-    PCA9685_SetLevel(M1_REV_CHANNEL, 0);
-    PCA9685_SetLevel(M2_REV_CHANNEL, 0);
+    stop();
+    usleep(1000000);  // Sleep for 1 second
 
+    printf("Turning right\n");
+    turn_right();
+    usleep(1000000);  // Sleep for 1 second
+
+    printf("Turning left\n");
+    turn_left();
     usleep(1000000);  // Sleep for 1 second
 
     pressed = 1;
@@ -79,7 +119,7 @@ int main() {
   PCA_Init();
   gpioSetMode(BUTTON_PIN, PI_INPUT);
   gpioSetPullUpDown(BUTTON_PIN, PI_PUD_UP);
-  if (gpioSetAlertFunc(BUTTON_PIN, MotorControlSequence)) {
+  if (gpioSetAlertFunc(BUTTON_PIN, motor_control_sequence)) {
     printf("Failed to set alert function! Exiting.\n");
     exit(0);
   }
@@ -91,12 +131,7 @@ int main() {
   // Clean up and exit
   printf("Cleaning up and exiting\n");
   gpioTerminate();
-  PCA9685_SetLevel(M1_FWD_CHANNEL, 0);
-  PCA9685_SetLevel(M2_FWD_CHANNEL, 0);
-  PCA9685_SetLevel(M1_REV_CHANNEL, 0);
-  PCA9685_SetLevel(M2_REV_CHANNEL, 0);
-  PCA9685_SetLevel(PWM_CHANNEL_A, 0);
-  PCA9685_SetLevel(PWM_CHANNEL_B, 0);
+  stop();  // Stop motors before exiting
   DEV_ModuleExit();
 
   return 0;
