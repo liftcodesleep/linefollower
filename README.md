3782574b5157a6718f82c3cfbc5ab6af0bfe7cb5
liftcodesleep <liftcodesleep@gmail.com>
2024-06-13 19:47:39 -0700

    fix: remove dupe src, fix error check on init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# line_follower
- 
+I built this project to run a line following and obstacle avoiding robot.
+
+The robot hardware I used can be found here: [The perseids DIY Robot Smart Car Chassis Kit for Raspberry Pi](https://www.amazon.com/gp/product/B07DNXBFQN)
+
+The computer I used was a [Raspberry Pi model B](https://www.raspberrypi.com/products/raspberry-pi-4-model-b/)
+
+An array of three [TCRT5000 IR sensors](https://www.amazon.com/HiLetgo-Channel-Tracing-Sensor-Detection/dp/B00LZV1V10) and one [HC-SR04 ultrasonic sensor](https://www.amazon.com/EPLZON-HC-SR04-Ultrasonic-Distance-Arduino/dp/B09PG4HTT1/) were used to follow the line and detect obstacles, respectively.
+
+There were also external batteries used to power the Pi and the motors.
+
+To run the program, build the car and wire it according to main.h, or whatever your preference provided you update the file as necessary.</br>
+Clone the repository directly onto the Raspberry Pi, then cd into /src and run 'make' and then sudo ./main</br>
+At this point the car should begin to run a course provided it detects one, or search for one if it does not.
+
+Some notes:
+The run can be linked to the press of a button by uncommenting the debounce block near the start of main()
+
+The code is somewhat hardware specific, abstractions were made where feasible, but it's possible that with different motors or batteries the timings passed to the motion functions will need to be adjusted.
+
+The performance of the HC-SR04 sensor can be dramatically improved with the use of makeshift paper tubes as described in [this excellent article](https://www.davidpilling.com/wiki/index.php/HCSR04)</br>
+The tubes were sufficient to negate the need for a larger sonic sensor array, but did create failure modes where when the car would bounce on uneven terrain the tubes would shift in such a way as to seem to indicate an obsacle. A sturdier connection than the electric tape I used would likely prevent this.
--- src/init.c
+++ src/init.c
@@ -3,74 +3,80 @@
 #include <string.h>
 #include <unistd.h>
 
-void update_remote_url(const char *repository_directory) {
-    char command[1024];
+void update_remote_url() {
+  char command[1024];
+  char buffer[1024];
 
-    // Get the current remote URL
-    snprintf(command, sizeof(command), "git config --get remote.origin.url");
-    FILE *remoteURL = popen(command, "r");
+  // Get the current remote URL
+  FILE *remoteURL = popen("git config --get remote.origin.url", "r");
+  if (remoteURL == NULL) {
+    perror("Error opening pipe for remote URL");
+    exit(EXIT_FAILURE);
+  }
 
-    if (remoteURL == NULL) {
-        perror("Error opening pipe for remote URL");
-        exit(EXIT_FAILURE);
-    }
-
-    char buffer[1024];
-    fgets(buffer, sizeof(buffer), remoteURL);
-
-    // Check if the remote URL uses HTTPS
-    if (strstr(buffer, "https://") != NULL) {
-        // Replace HTTPS with SSH in the remote URL
-        snprintf(command, sizeof(command), "git remote set-url origin git@github.com:%s",
-                 buffer + strlen("https://"));
-
-        // Update the remote URL to use SSH
-        if (system(command) == -1) {
-            perror("Error updating remote URL");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Updated remote URL to: git@github.com:%s\n", buffer + strlen("https://"));
-    }
-
+  if (fgets(buffer, sizeof(buffer), remoteURL) == NULL) {
+    perror("Error reading remote URL");
     pclose(remoteURL);
+    exit(EXIT_FAILURE);
+  }
+  pclose(remoteURL);
+
+  // Remove trailing newline character
+  buffer[strcspn(buffer, "\n")] = 0;
+
+  // Check if the remote URL uses HTTPS
+  if (strstr(buffer, "https://") != NULL) {
+    // Replace HTTPS with SSH in the remote URL
+    snprintf(command, sizeof(command),
+             "git remote set-url origin git@github.com:%s",
+             buffer + strlen("https://"));
+
+    // Update the remote URL to use SSH
+    if (system(command) == -1) {
+      perror("Error updating remote URL");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("Updated remote URL to: git@github.com:%s\n",
+           buffer + strlen("https://"));
+  }
 }
 
 void git_pull(const char *branch) {
-    char command[1024];
+  char command[1024];
 
-    // Get the current working directory
-    char current_directory[1024];
-    if (getcwd(current_directory, sizeof(current_directory)) == NULL) {
-        perror("Error getting current working directory");
-        exit(EXIT_FAILURE);
-    }
+  // Get the current working directory
+  char current_directory[1024];
+  if (getcwd(current_directory, sizeof(current_directory)) == NULL) {
+    perror("Error getting current working directory");
+    exit(EXIT_FAILURE);
+  }
 
-    // Update the remote URL if needed
-    update_remote_url(current_directory);
+  // Update the remote URL if needed
+  update_remote_url();
 
-    // Perform git pull in the current directory
-    snprintf(command, sizeof(command), "git checkout %s", branch);
-    if (system(command) == -1) {
-        perror("Error checking out branch");
-        exit(EXIT_FAILURE);
-    }
+  // Perform git pull in the current directory
+  snprintf(command, sizeof(command), "git checkout %s", branch);
+  if (system(command) == -1) {
+    perror("Error checking out branch");
+    exit(EXIT_FAILURE);
+  }
 
-    snprintf(command, sizeof(command), "git pull origin %s", branch);
-    if (system(command) == -1) {
-        perror("Error pulling changes");
-        exit(EXIT_FAILURE);
-    }
+  snprintf(command, sizeof(command), "git pull origin %s", branch);
+  if (system(command) == -1) {
+    perror("Error pulling changes");
+    exit(EXIT_FAILURE);
+  }
 
-    printf("Git pull successful!\n");
+  printf("Git pull successful!\n");
 }
 
 int main() {
-    // Specify the branch you want to pull (default is "main")
-    const char *branch = "main";
+  // Specify the branch you want to pull (default is "main")
+  const char *branch = "main";
 
-    // Call the function to perform git pull
-    git_pull(branch);
+  // Call the function to perform git pull
+  git_pull(branch);
 
-    return 0;
+  return 0;
 }
